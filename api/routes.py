2db616f91ba6e4c6e41116cd41721d7ec89f41f5
v1b3m <vibenjamin6@gmail.com>
2018-12-20 12:14:10 +0300

    endpoint to get specific redflag

---
+++ api/routes.py
@@ -0,0 +1,49 @@
+from api import app
+from flask import jsonify, request, json
+
+redflags = [{
+            "id": 1,
+            "createdOn" : "2018-21-23 21:15",
+            "createdBy" : "Benjamin",
+            "type" : "red-flag",
+            "location" : {"lat":"0.96","long":"1.23"},
+            "status" : "Pending",
+            "Images" : ["me.jpg"],
+            "Videos": ['ben.mp4'],
+            "comment": "I am the greatest" },
+            {
+            "id": 2,
+            "createdOn" : "2014-21-23 21:15",
+            "createdBy" : "Breezy",
+            "type" : "red-flag",
+            "location" : {"lat":"0.96","long":"1.23"},
+            "status" : "Approved",
+            "Images" : ["me.jpg"],
+            "Videos": ['ben.mp4'],
+            "comment": "I am the bestest" }
+            ]
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+@app.route('/api/v1/red-flags',)
+def get_all_redflags():
+    red_flags = redflags
+
+    if len(red_flags) < 1:
+        return jsonify({'message': 'There are no redflags'}), 400
+    return jsonify({"status": 200,
+                    "data": red_flags})
+
+@app.route('/api/v1/red-flags/<int:id>')
+def get_specific_redflag(id):
+    redflag = None
+    for item in redflags:
+        if item["id"] == id:
+            redflag = item
+    if redflag is None: 
+        return jsonify({'message': "The redflag doesn't exist"}), 404    
+    return jsonify({"status": 200,
+                    "data": redflag})
