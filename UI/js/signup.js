046554f1efe2988d828da281201f23e4766d50fc
v1b3m <vibenjamin6@gmail.com>
2019-02-04 09:38:18 +0300

    Remove unwanted files

--- UI/js/signup.js
+++ UI/js/signup.js
@@ -1,6 +1,6 @@
 document.getElementById('registration-form').addEventListener('submit', registerNewUser)
 
-const url ='http://127.0.0.1:5000/auth/register'
+const url ='https://andelaireporterapp.herokuapp.com/auth/register'
 
 function registerNewUser(event) {
     event.preventDefault()
