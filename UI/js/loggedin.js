6655ce27585ea58b3d421468c79963a6bc9d91c4
v1b3m <vibenjamin6@gmail.com>
2019-02-07 08:27:25 +0300

    Revert master back

--- UI/js/loggedin.js
+++
@@ -1,9 +0,0 @@
-
-function isLoggedIn(){
-    var token = localStorage.getItem('token')
-    if (token == null || token === '') {
-        window.location.replace('./signin.html')
-    }
-}
-
-isLoggedIn()
--- UI/js/responsive.js
+++
@@ -1,8 +0,0 @@
-function makeResponsive() {
-    var i = document.getElementById("mynavbar");
-    if (i.className == "navbar") {
-        i.className += " responsive";
-    } else {
-        i.className = "navbar";
-    }
-}
